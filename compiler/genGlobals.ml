a6de22817c9e75cf2cf7c0e0e4614fe58a5bc0e5
sam <samg@fastmail.org>
2021-11-11 08:27:42 -0800

    hard code genGlobals for now

---
+++ compiler/genGlobals.ml
@@ -0,0 +1,12 @@
+(* If this is needed in the future, it should probably come from 
+   the configure script *)
+let arch_type = "ARCH_64"
+let tramp_size = 16
+let tramp_align = 8
+let max_reg_return = 4
+let data_layout = "s0:64:64"
+let llc = ""
+let opt = ""
+let stdlibdir = "../stdlib"
+let cc = "cc"
+let ar = "ar"
